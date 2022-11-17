0d4e01906bf514e91150582e43d7a2e7550d52a5
ENDRIO OLIVEIRA DE JESUS <endrio.ojesus@senacsp.edu.br>
2022-11-17 21:27:28 -0200

    Aula 17/11

--- js/renderFavorites.js
+++ js/renderFavorites.js
@@ -1,63 +1,11 @@
 const favorites = sessionStorage.getItem("favoritos").split("%");
-const favoritesTable = document.getElementById("favorites_table");
-const favoritesArray = [];
-for (let i = 0; i < favorites.length; i++) {
-    const object = JSON.parse(favorites[i])
-    favoritesArray.push(object);
-}
-for (let i = 0; i < favoritesArray.length; i++) {
-    favoritesTable.innerHTML = favoritesTable.innerHTML + `
-    <tr>
-        <td>
-            <div class="media">
-            <div class="media-body">
-                <img class="favorite-img" src="${favoritesArray[i].img}">
-            </div>
-            </div>
-        </td>
-        <td>
-            <div class="product_count">
-            <p>${favoritesArray[i].name}</p>
-            </div>
-        </td>
-        <td>
-            <div class="product_count">
-            <p>${favoritesArray[i].area}</p>
-            </div>
-        </td>
-        <td>
-            <div class="product_count">
-            <p>${favoritesArray[i].quartos}</p>
-            </div>
-        </td>
-        <td>
-            <a href="#" onclick="deletar(${[i]})" class="genric-btn danger circle">[X]</a>
-        </td>
-    </tr>`
-}
-
-function deletar(id) {
-    for (let i = 0; i < favorites.length; i++) {
-        if([i] == id){
-            favorites.splice(i, 1);
-        }        
-    }
-    const remainFavorites = favorites;
-    
-    /* Atualizar cache para nova lista de favoritos */
-    const favoritesCache = sessionStorage.setItem("favoritos", "")
-    for (let i = 0; i < remainFavorites.length; i++) {
-        const m = sessionStorage.setItem("favoritos", `${JSON.stringify(remainFavorites[i])}%`)
-        console.log(m);
-    }
-
+function render(){
+    const favoritesTable = document.getElementById("favorites_table");
     const favoritesArray = [];
-    for (let i = 0; i < remainFavorites.length; i++) {
-        const object = JSON.parse(remainFavorites[i])
+    for (let i = 0; i < favorites.length; i++) {
+        const object = JSON.parse(favorites[i])
         favoritesArray.push(object);
     }
-    const favoritesTable = document.getElementById("favorites_table");
-    favoritesTable.innerHTML = "";
     for (let i = 0; i < favoritesArray.length; i++) {
         favoritesTable.innerHTML = favoritesTable.innerHTML + `
         <tr>
@@ -88,4 +36,64 @@ function deletar(id) {
             </td>
         </tr>`
     }
+}
+render();
+
+function deletar(id) {
+    const favoritesArr = [];
+    for (let i = 0; i < favorites.length; i++) {
+        if(i == id){
+            console.log(true);
+        }else{
+            favoritesArr.push(JSON.parse(favorites[i]));      
+        }
+    }
+    var newList = "";
+
+    for (let i = 0; i < favoritesArr.length; i++) {
+        newList = newList + JSON.stringify(favoritesArr[i]) + "%"   
+    }
+
+    sessionStorage.removeItem("favoritos");
+    sessionStorage.setItem("favoritos", newList);
+
+    const favoritesTable = document.getElementById("favorites_table");
+    const newFavorites = sessionStorage.getItem("favoritos").split("%")
+    const newFavoritesArr = [];
+
+    for (let i = 0; i < newFavorites.length - 1; i++) {
+        const object = JSON.parse(newFavorites[i])
+        newFavoritesArr.push(object);
+    }
+    console.log(newFavoritesArr);
+    for (let i = 0; i < newFavoritesArr.length; i++) {
+        favoritesTable.innerHTML = favoritesTable.innerHTML + `
+        <tr>
+            <td>
+                <div class="media">
+                <div class="media-body">
+                    <img class="favorite-img" src="${newFavoritesArr[i].img}">
+                </div>
+                </div>
+            </td>
+            <td>
+                <div class="product_count">
+                <p>${newFavoritesArr[i].name}</p>
+                </div>
+            </td>
+            <td>
+                <div class="product_count">
+                <p>${newFavoritesArr[i].area}</p>
+                </div>
+            </td>
+            <td>
+                <div class="product_count">
+                <p>${newFavoritesArr[i].quartos}</p>
+                </div>
+            </td>
+            <td>
+                <a href="#" onclick="deletar(${[i]})" class="genric-btn danger circle">[X]</a>
+            </td>
+        </tr>`
+    }    
 }
